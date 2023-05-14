00fc1fd40041097fe0630c997982e92bfc2d56b8
Jove-2761781 <jove2761781@outlook.com>
2023-05-14 18:58:54 +0800

    Site updated: 2023-05-14 18:58:25

---
+++ asset/getindex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /mnt/d/blog/source/asset/html || exit
+idx_html=index.html
+cat /dev/null > $idx_html
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>HTML</title></head><body><h1>Oracle Patch README</h1><ul>' > $idx_html
+for i in *.html; do
+  [[ -e "$i" ]] || break
+  if [ "$i" != 'index.html' ]; then
+    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_html
+  fi
+done
+echo '</ul></body></html>' >> $idx_html
+echo 'HTML首页文件生成完毕'
+
+cd /mnt/d/blog/source/asset/pdf || exit
+idx_pdf=index.html
+cat /dev/null > $idx_pdf
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>PDF</title></head><body><h1>Oracle MOS Documents</h1><ul>' > $idx_pdf
+for i in *.pdf; do
+  [[ -e "$i" ]] || break
+  if [ "$i" != 'index.html' ]; then
+    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_pdf
+  fi
+done
+echo '</ul></body></html>' >> $idx_pdf
+echo 'PDF首页文件生成完毕'
