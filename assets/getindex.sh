fa864990f2d22a0f49e723e919ea8579a529fd6b
Jove-2761781 <jove2761781@outlook.com>
2023-05-18 09:02:26 +0800

    Site updated: 2023-05-18 09:01:23

--- assets/getindex.sh
+++ assets/getindex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /mnt/d/blog/source/asset/html || exit
+cd /d/blog/source/assets/html || exit
 idx_html=index.html
 cat /dev/null > $idx_html
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>HTML</title></head><body><h1>Oracle Patch README</h1><ul>' > $idx_html
@@ -13,7 +13,7 @@ done
 echo '</ul></body></html>' >> $idx_html
 echo 'HTML首页文件生成完毕'
 
-cd /mnt/d/blog/source/asset/pdf || exit
+cd /d/blog/source/assets/pdf || exit
 idx_pdf=index.html
 cat /dev/null > $idx_pdf
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>PDF</title></head><body><h1>Oracle MOS Documents</h1><ul>' > $idx_pdf
