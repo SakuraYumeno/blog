4365b0782c8759bbdfeec1994d1752d3b0278530
Jove-2761781 <jove2761781@outlook.com>
2023-05-28 23:45:22 +0800

    Site updated: 2023-05-28 23:44:47

--- assets/getindex.sh
+++ assets/getindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /d/blog/source/assets/html || exit
 idx_html=index.html
+cd /d/blog/source/assets/html || exit
 cat /dev/null > $idx_html
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>HTML</title></head><body><h1>Oracle Patch README</h1><ul>' > $idx_html
 for i in *.html; do
@@ -11,17 +11,28 @@ for i in *.html; do
   fi
 done
 echo '</ul></body></html>' >> $idx_html
-echo 'HTML首页文件生成完毕'
+echo 'HTML索引文件生成完毕'
 
 cd /d/blog/source/assets/pdf || exit
-idx_pdf=index.html
-cat /dev/null > $idx_pdf
-echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>PDF</title></head><body><h1>Oracle MOS Documents</h1><ul>' > $idx_pdf
+cat /dev/null > $idx_html
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>PDF</title></head><body><h1>Oracle MOS Documents</h1><ul>' > $idx_html
 for i in *.pdf; do
   [[ -e "$i" ]] || break
   if [ "$i" != 'index.html' ]; then
-    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_pdf
+    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_html
   fi
 done
-echo '</ul></body></html>' >> $idx_pdf
-echo 'PDF首页文件生成完毕'
+echo '</ul></body></html>' >> $idx_html
+echo 'PDF索引文件生成完毕'
+
+cd /d/blog/source/assets/script || exit
+cat /dev/null > $idx_html
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>Script</title></head><body><h1>Script</h1><ul>' > $idx_html
+for i in *.sh; do
+  [[ -e "$i" ]] || break
+  if [ "$i" != 'index.html' ]; then
+    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_html
+  fi
+done
+echo '</ul></body></html>' >> $idx_html
+echo 'Script索引文件生成完毕'
