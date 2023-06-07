4ccbc5008e94405c4ecae24c3026ab72569379d9
Jove-2761781 <jove2761781@outlook.com>
2023-06-07 17:31:23 +0800

    Site updated: 2023-06-07 17:30:51

--- assets/getindex.sh
+++ assets/getindex.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 idx_html=index.html
+
+cd /d/blog/source/assets/ || exit
+for f in */**/*\ *; do mv "$f" "${f// /-}" 2>/dev/null; done
+
 cd /d/blog/source/assets/html || exit
 cat /dev/null > $idx_html
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>HTML</title></head><body><h1>Oracle Patch README</h1><ul>' > $idx_html
@@ -16,10 +20,21 @@ echo 'HTML索引文件生成完毕'
 cd /d/blog/source/assets/pdf || exit
 cat /dev/null > $idx_html
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>PDF</title></head><body><h1>Oracle MOS Documents</h1><ul>' > $idx_html
-for i in *.pdf; do
+for i in oracle/*.pdf; do
   [[ -e "$i" ]] || break
   if [ "$i" != 'index.html' ]; then
-    echo '<li><a href="'$i'">'$i'</a></li>' >> $idx_html
+    i2=$(basename $i)
+    echo '<li><a href="'$i'">'$i2'</a></li>' >> $idx_html
+  fi
+done
+echo '</ul>' >> $idx_html
+echo "" >> $idx_html
+echo '<h1>Linux Documents</h1><ul>' >> $idx_html
+for i in linux/*.pdf; do
+  [[ -e "$i" ]] || break
+  if [ "$i" != 'index.html' ]; then
+    i2=$(basename $i)
+    echo '<li><a href="'$i'">'$i2'</a></li>' >> $idx_html
   fi
 done
 echo '</ul></body></html>' >> $idx_html
